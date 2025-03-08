acb62a35a7f6a13848c449428bcc72e4c4332b87
Сергей Голобоков <sergeygolobokov@gmail.com>
2025-03-09 01:39:47 +0300

    Add book model

--- internal/models/book.go
+++ internal/models/book.go
@@ -1,1 +1,9 @@
 package models
+
+type Book struct {
+	ID          uint64 `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	CreatedAt   int    `json:"year"`
+	Author      string `json:"author"`
+}
