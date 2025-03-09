a88eeab4ea246cce46321ce1c7a6497ac948f218
Сергей Голобоков <sergeygolobokov@gmail.com>
2025-03-09 14:54:36 +0300

    Add CheckDB and closeDB for SessionDB

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -1,36 +1,51 @@
 package storage
 
 import (
+	"book/internal/storage/queries"
 	"database/sql"
 	"fmt"
-
-	_ "modernc.org/sqlite"
-
-	"book/internal/storage/queries"
 )
 
-func InitDB(storagePath string) (*sql.DB, error) {
-	const op = "storage.sqlite.Init"
-	var db *sql.DB
+const op = "storage.sqlite.Init"
 
-	db, err := sql.Open("sqlite", storagePath)
+func CheckDB(db *sql.DB, err error) error {
 	if err != nil {
-		return nil, fmt.Errorf("%s: failed to open sqlite3 file %w", op, err)
+		return fmt.Errorf("%s: failed to open sqlite3 file %w", op, err)
 	}
 
 	err = db.Ping()
 	if err != nil {
-		return nil, fmt.Errorf("%s: failed to ping database: %w", op, err)
+		return fmt.Errorf("%s: failed to ping database: %w", op, err)
+	}
+
+	return nil
+}
+
+func CloseDB(db *sql.DB) error {
+	err := db.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func SessionDB(storagePath string) (*sql.DB, error) {
+	var db *sql.DB
+
+	db, err := sql.Open("sqlite", storagePath)
+	ok := CheckDB(db, err)
+	if ok != nil {
+		return nil, ok
 	}
 
 	stmt, err := db.Prepare(queries.CreateBookTable)
 	if err != nil {
-		return nil, fmt.Errorf("%s: failed to prepare query - %w", op, err)
+		return nil, fmt.Errorf("%s: failed to prepare book table query - %w", op, err)
 	}
 
 	_, err = stmt.Exec()
 	if err != nil {
-		return nil, fmt.Errorf("%s: failed to execute query - %w", op, err)
+		return nil, fmt.Errorf("%s: failed book table - %w", op, err)
 	}
 
 	return db, nil
